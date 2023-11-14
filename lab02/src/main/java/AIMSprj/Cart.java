855909ac7dad1fbbc9ad5357d358308f2bce60db
Phạm Vân Anh <pvanh102@gmail.com>
2023-11-14 20:57:08 +0700

    lab02

---
+++ lab02/src/main/java/AIMSprj/Cart.java
@@ -0,0 +1,54 @@
+package AIMSprj;
+
+public class Cart {
+
+    // Max Number of order
+    public static final int MAX_NUMBERS_ORDERED = 20;
+
+    // array store orders
+    private DigitalVideoDisc itemsOrdered[] =
+            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+
+    // qtyOrdered: how many DigitalVideoDiscs are in the cart
+    private int qtyOrdered = 0;
+
+    // add an item to the list
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        // check the current quantity to assure that the cart is not already full
+        if(qtyOrdered < 20) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        }
+        else {
+            System.out.println("The cart is almost full");
+        }
+    }
+
+
+
+    // remove the item passed by argument from the list
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+
+        for(int index = 0; index < qtyOrdered; index++) {
+            if(itemsOrdered[index].getTitle() == disc.getTitle()) {
+
+                for(int j = index; j < qtyOrdered; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                qtyOrdered--;
+                System.out.println("The disc has been removed");
+                break;
+            }
+        }
+    }
+
+    // method which loops through the values of the array and sums the costs of the individual DigitalVideoDiscs
+    public float totalCost() {
+        float total = 0;
+        for(int index = 0; index < qtyOrdered; index++) {
+            total += itemsOrdered[index].getCost();
+        }
+        return total;
+    }
+}
---
+++ lab02/src/main/java/AIMSprj/DigitalVideoDisc.java
@@ -0,0 +1,85 @@
+package AIMSprj;
+
+public class DigitalVideoDisc {
+    // Attributes
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    // accessors and mutators for the class DigitalVideoDisc
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    // Constructor
+
+    // Create a DVD object by title
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    // Create a DVD object by category, title and cost
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    // Create a DVD object by director, category, title and cost
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    // Create a DVD object by all attributes: title, category, director, length and cost
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+}
+
+
