923e975c1ce924132ad4291e69ed59dd52d28798
Phạm Vân Anh <pvanh102@gmail.com>
2023-11-15 17:11:48 +0700

    topic/passing-parameter

--- lab02/src/main/java/AIMSprj/TestPassingParameter.java
+++ lab02/src/main/java/AIMSprj/TestPassingParameter.java
@@ -6,7 +6,13 @@ public class TestPassingParameter {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
-        swap(jungleDVD, cinderellaDVD);
+//        swap(jungleDVD, cinderellaDVD);
+//        System.out.println("Swap wrong!");
+//        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+//        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        correctlySwap(jungleDVD, cinderellaDVD);
+        System.out.println("Swap correctly!");
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
         System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
@@ -22,9 +28,31 @@ public class TestPassingParameter {
         dvd = new DigitalVideoDisc(oldTitle);
     }
 
-    private static void swap(Object o1, Object o2) {
-        Object tmp = o1;
-        o1 = o2;
-        o2 = tmp;
+//    private static void swap(Object o1, Object o2) {
+//        Object tmp = o1;
+//        o1 = o2;
+//        o2 = tmp;
+//    }
+
+    public static void correctlySwap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2){
+        DigitalVideoDisc tmp = new DigitalVideoDisc(
+                dvd1.getTitle(),
+                dvd2.getCategory(),
+                dvd1.getDirector(),
+                dvd1.getLength(),
+                dvd1.getCost()
+        );
+
+        dvd1.setTitle(dvd2.getTitle());
+        dvd1.setCategory(dvd2.getCategory());
+        dvd1.setDirector(dvd2.getDirector());
+        dvd1.setLength(dvd2.getLength());
+        dvd1.setCost(dvd2.getCost());
+
+        dvd2.setTitle(tmp.getTitle());
+        dvd2.setCategory(tmp.getCategory());
+        dvd2.setDirector(tmp.getDirector());
+        dvd2.setLength(tmp.getLength());
+        dvd2.setCost(tmp.getCost());
     }
 }
