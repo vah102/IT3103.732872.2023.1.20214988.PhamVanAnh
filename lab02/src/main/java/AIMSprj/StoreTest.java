6f3d6b83e7212c68f35e36775d3d8ba35d65eaa4
Phạm Vân Anh <pvanh102@gmail.com>
2023-11-15 23:17:25 +0700

    topic/store

--- lab02/src/main/java/AIMSprj/StoreTest.java
+++ lab02/src/main/java/AIMSprj/StoreTest.java
@@ -1,23 +1,30 @@
 package AIMSprj;
 
 public class StoreTest {
-    public static void main(String[] args){
-        //TODO Auto-generated method stub
+
+    public static void main(String[] args) {
         Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation",
+                "Roger Allers",87,19.95f);
+        // Khoi tao doi tuong dvd 1 voi cac gia tri nhu tren
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
-                "Animation", "Roger Allers", 87, 19.95f);
-        store.addDVD(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction",
+                "Geogre Lucas",84,24.95f);
+        // Khoi tao doi tuong dvd 2 voi cac gia tri nhu tren
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
-                "Science Fiction", "George Lucas", 87, 24.95f);
-        store.addDVD(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        // Khoi tao doi tuong dvd 3 voi cac gia tri nhu tren
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
-                "Animation", 18.99f);
-        store.addDVD(dvd3);
-
-        store.removeDVD(dvd2);
-//
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("The Bird King","Animation",
+                "Roger Allers",87,19.95f);
+        // Khoi tao doi tuong dvd 1 voi cac gia tri nhu tren
+        store.removeDvd(dvd3);  // no disc
+        store.addDvd(dvd1);    //add
+        store.addDvd(dvd2);//add
+        store.addDvd(dvd3);//add
+        store.removeDvd(dvd3);// remove success
+        store.removeDvd(dvd4); // not found to remove
     }
+
+
 }
